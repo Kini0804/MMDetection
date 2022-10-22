55cf57c160ef83564bfb6ea4d0bdbef2f0d27ea0
刘少冬 <liushaodong@lsd.local>
2022-10-22 09:00:02 +0800

    add

--- mmdet/core/evaluation/class_names.py
+++ mmdet/core/evaluation/class_names.py
@@ -7,10 +7,14 @@ def wider_face_classes():
 
 
 def voc_classes():
+    # return [
+    #     'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
+    #     'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person',
+    #     'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+    # ]
     return [
-        'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
-        'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person',
-        'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+        'ore carrier', 'bulk cargo carrier', 'general cargo ship', 'container ship',
+        'fishing boat', 'passenger ship'
     ]
 
 
